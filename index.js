f482cda734f6f2ecc95b93828fc82ba97adf8845
mastercoms <circuitsoft@tuta.io>
2017-10-23 19:44:25 -0400

    token API

---
+++ index.js
@@ -0,0 +1,26 @@
+var admin = require("firebase-admin");
+var express = require("express");
+var cors = require("cors");
+var app = express();
+var port = 80;
+
+var serviceAccount = require("/opt/firebase/key.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://versutianpolls.firebaseio.com"
+});
+
+app.use(cors());
+app.listen(port);
+
+app.get('/token', function(req, res) {
+  var uid = req.query.nation;
+  admin.auth().createCustomToken(uid)
+    .then(function(token) {
+      res.send(token);
+    })
+    .catch(function(error) {
+      console.log("Error creating token:", error);
+    });
+});
