47cbe060171b132ca9550d4162aa55cbda94a212
mastercoms <circuitsoft@tuta.io>
2017-11-07 13:43:18 -0500

    remove hostname

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var express = require("express");
 var cors = require("cors");
 var app = express();
 var port = 80;
-var hostname = 'api.versutian.site';
 
 var serviceAccount = require("/opt/firebase/key.json");
 
@@ -13,7 +12,7 @@ admin.initializeApp({
 });
 
 app.use(cors());
-app.listen(port, hostname);
+app.listen(port);
 
 app.get('/token', function(req, res) {
   var uid = req.query.nation;
